c7a385a9f6a5a89ba2390e467e81488cd2787256
安德鲁 <andrew@zhuitech.com>
2019-04-27 20:37:00 +0800

    upgrade ibrand backend

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,34 +6,17 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ Admin::title() }}</title>
     <meta name="_token" content="{{ csrf_token() }}"/>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>{{ Admin::title() }}</title>
     <link rel="shortcut icon" href="{{ asset($dmp_config['shortcut_icon'] ?? '') }}" type="image/x-icon" />
 
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css") }}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/font-awesome/css/font-awesome.min.css") }}">
-
-    <link rel="stylesheet" href="//at.alicdn.com/t/font_u5095o4vzog8pvi.css">
-    <!-- Theme style -->
-    {{--<link rel="stylesheet"
-          href="{{ admin_asset("/vendor/laravel-admin/AdminLTE/dist/css/skins/" . config('admin.skin') .".min.css") }}">--}}
-
     {!! Admin::css() !!}
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/laravel-admin/laravel-admin.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/nprogress/nprogress.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/sweetalert/dist/sweetalert.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/nestable/nestable.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/toastr/build/toastr.min.css") }}">
-    <link rel="stylesheet"
-          href="{{ admin_asset("/vendor/laravel-admin/bootstrap3-editable/css/bootstrap-editable.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/google-fonts/fonts.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/AdminLTE/dist/css/AdminLTE.min.css") }}">
 
     <!-- iconfont CSS-->
     <link rel="stylesheet" href="//at.alicdn.com/t/font_u5095o4vzog8pvi.css">
+
     <!-- REQUIRED CSS BY iBrand-->
     <link rel="stylesheet" href="{{ admin_asset ("/vendor/libs/webuploader-0.1.5/webuploader.css") }}">
     <link rel="stylesheet" href="{{ admin_asset("/vendor/inspinia/css/animate.css") }}">
@@ -41,14 +24,10 @@
     <link rel="stylesheet" href="{{ admin_asset("/vendor/inspinia/css/main.css") }}">
     <link rel="stylesheet" href="{{ admin_asset("/vendor/inspinia/css/admin.css") }}">
     <link rel="stylesheet" href="{{ admin_asset("/vendor/css/plugins/iCheck/custom.css") }}">
+    <link rel="stylesheet" href="{{ admin_asset("/vendor/boot-admin/css/style.css") }}">
 
     <!-- REQUIRED JS SCRIPTS -->
-    <script src="{{ admin_asset ("/vendor/laravel-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/laravel-admin/AdminLTE/bootstrap/js/bootstrap.min.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/laravel-admin/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/laravel-admin/AdminLTE/dist/js/app.min.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/laravel-admin/jquery-pjax/jquery.pjax.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/laravel-admin/nprogress/nprogress.js") }}"></script>
+    <script src="{{ Admin::jQuery() }}"></script>
     <script src="{{ admin_asset ("/vendor/libs/jquery.form.min.js") }}"></script>
 
     {!! \iBrand\Backend\Backend::js() !!}
@@ -95,16 +74,6 @@
         </div>
     </div>
 
-    {{--@include('admin::partials.header')--}}
-
-    {{--@include('admin::partials.sidebar')--}}
-
-    {{--<div class="content-wrapper" id="pjax-container">
-        @yield('content')
-        {!! Admin::script() !!}
-    </div>--}}
-
-    {{--@include('admin::partials.footer')--}}
     <div id="modal" class="modal inmodal fade"></div>
 </div>
 
@@ -117,23 +86,15 @@
 </script>
 
 <!-- REQUIRED JS SCRIPTS -->
-<script src="{{ admin_asset("/vendor/laravel-admin/nestable/jquery.nestable.js") }}"></script>
-<script src="{{ admin_asset("/vendor/laravel-admin/toastr/build/toastr.min.js") }}"></script>
-<script src="{{ admin_asset("/vendor/laravel-admin/bootstrap3-editable/js/bootstrap-editable.min.js") }}"></script>
-<script src="{{ admin_asset("/vendor/laravel-admin/sweetalert/dist/sweetalert.min.js") }}"></script>
 {!! Admin::js() !!}
-{{--<script src="{{ admin_asset ("/vendor/laravel-admin/laravel-admin/laravel-admin.js") }}"></script>--}}
 
 <!-- REQUIRED JS SCRIPTS BY iBrand-->
-<script src="{{ admin_asset("/vendor/libs/jquery.form.min.js") }}"></script>
-<script src="{{ admin_asset("/vendor/libs/webuploader-0.1.5/webuploader.js") }}"></script>
+<script src="{{ admin_asset ("/vendor/libs/webuploader-0.1.5/webuploader.js") }}"></script>
 <script src="{{ admin_asset("/vendor/inspinia/js/plugins/metisMenu/jquery.metisMenu.js") }}"></script>
 <script src="{{ admin_asset("/vendor/inspinia/js/inspinia.js") }}"></script>
-<script src="{{ admin_asset("/vendor/inspinia/js/plugins/pace/pace.min.js") }}"></script>
-<script src="{{ admin_asset("/vendor/libs/laravel-admin.js") }}"></script>
-<script src="{{ admin_asset("/vendor/libs/icheck.min.js") }}"></script>
-<script src="{{ admin_asset("/vendor/libs/plugins.js") }}"></script>
-<script src="{{ admin_asset("/vendor/libs/active.js") }}"></script>
+<script src="{{ admin_asset ("/vendor/libs/icheck.min.js") }}"></script>
+<script src="{{ admin_asset ("/vendor/libs/plugins.js") }}"></script>
+<script src="{{ admin_asset ("/vendor/libs/active.js") }}"></script>
 
 <script type="text/javascript">
     $(function () {
--- resources/views/admin/partials/top-menu.blade.php
+++ resources/views/admin/partials/top-menu.blade.php
@@ -1,8 +1,8 @@
-@if(Admin::user()->visible($item['roles']))
-<li class="{{$item['class']}}">
-    <a href="{{ admin_base_path($item['uri']) }}" no-pjax>
-        <i class="fa {{$item['icon']}}"></i>
-        <span>{{$item['title']}}</span>
-    </a>
-</li>
+@if(Admin::user()->visible($item['roles']) && (empty($item['permission']) ?: Admin::user()->can($item['permission'])))
+    <li class="{{$item['class']}}">
+        <a target="_blank" href="{{ admin_base_path($item['uri']) }}">
+            <i class="fa {{$item['icon']}}"></i>
+            <span>{{$item['title']}}</span>
+        </a>
+    </li>
 @endif
