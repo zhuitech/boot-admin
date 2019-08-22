5e79f99264b747d62653a3548c822676ce0c66d7
安德鲁 <andrew@zhuitech.com>
2019-08-23 00:31:22 +0800

    proxy

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,5 @@
 <?php
 Route::group(['prefix' => 'svc', 'namespace' => 'ZhuiTech\BootAdmin\Admin\Controllers'], function () {
-    Route::get('/', 'ServiceController@index');
+    Route::get('/', 'ServiceController@index')->name('admin.svc.dashboard');
     Route::any('/{wildcard?}', 'ServiceController@admin')->where('wildcard', '.+');
 });
--- src/Admin/Controllers/ServiceController.php
+++ src/Admin/Controllers/ServiceController.php
@@ -5,9 +5,9 @@ namespace ZhuiTech\BootAdmin\Admin\Controllers;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
-use Illuminate\Support\Str;
 use ZhuiTech\BootAdmin\Controllers\AdminController;
-use ZhuiTech\BootLaravel\Helpers\RestClient;
+use ZhuiTech\BootLaravel\Helpers\ProxyClient;
+use ZhuiTech\BootLaravel\Models\User;
 
 class ServiceController extends AdminController
 {
@@ -16,47 +16,39 @@ class ServiceController extends AdminController
         return $content
             ->title('Dashboard')
             ->description('Description...')
-            ->row(function (Row $row) {
-            });
+            ->row(function (Row $row) {});
     }
 
+    /**
+     * 代理后台页面
+     *
+     * @return \GuzzleHttp\Psr7\Response|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function admin()
     {
         $request = request();
-        $user = Admin::user();
-        $contentType = $request->header('Content-Type');
 
-        $options = [
-            'headers' => [
-                'X-User' => $user->id,
-                'X-FORWARDED-PROTO' => $request->getScheme(),
-                'X-FORWARDED-HOST' => $request->server('HTTP_HOST'),
-            ],
-            'query' => $request->query(),
-        ];
-
-        foreach (['X-PJAX', 'X-PJAX-Container'] as $item) {
-            if ($request->hasHeader($item)) {
-                $options['headers'][$item] = $request->header($item);
-            }
+        // 不能直接访问内页
+        if ($request->method() == 'GET' && !$request->ajax()) {
+            return redirect(route('admin.svc.dashboard'));
         }
 
-        if (Str::contains($contentType, 'multipart/form-data')) {
-            $multipart = RestClient::server()->createMultipart($request->all());
-            $options['multipart'] = $multipart;
-        } else {
-            $options['headers']['Content-Type'] = $contentType;
-            $options['body'] = $request->getContent();
-        }
+        // 传递用户身份
+        $admin = new User([
+            'id' => Admin::user()->getAuthIdentifier(),
+            'type' => 'admins'
+        ]);
 
-        $result = RestClient::server('service')
-            ->plain()
-            ->request($request->path(), $request->method(), $options);
-
-        return $result;
+        return ProxyClient::server('service')->as($admin)->pass();
     }
 
+    /**
+     * 代理API
+     *
+     * @return \GuzzleHttp\Psr7\Response
+     */
     public function api()
     {
+        return ProxyClient::server('service')->pass();
     }
 }
