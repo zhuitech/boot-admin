22910dab6e61029c3246c2f8e215cc7186769493
安德鲁 <andrew@zhuitech.com>
2019-11-29 01:18:56 +0800

    excel import

--- src/Admin/Excel/ImportCollection.php
+++ src/Admin/Excel/ImportCollection.php
@@ -10,10 +10,37 @@ class ImportCollection implements ToCollection
     /**
      * @var Collection
      */
-    public $data;
+    public $data = [];
 
     public function collection(Collection $rows)
     {
-        $this->data = $rows;
+        foreach ($rows as $row) {
+            $this->data[] = $row->toArray();
+        }
+    }
+
+    /**
+     * 转换标题行为健名
+     * @return array
+     */
+    public function treatTitle()
+    {
+        $result = [];
+        $titles = [];
+
+        foreach ($this->data as $i => $row) {
+            if ($i === 0) {
+                $titles = $row;
+                continue;
+            }
+
+            $item = [];
+            foreach ($row as $j => $cell) {
+                $item[$titles[$j]] = $cell;
+            }
+            $result[] = $item;
+        }
+
+        return $result;
     }
 }
